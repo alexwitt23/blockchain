b0d4be2dee7137b566f6556227b98a3a8d0a9f14
hyeonseunglee <hyeonseung.lee@utexas.edu>
2021-05-06 18:19:38 -0500

    updates -- transaction_api.py

--- src/transaction_api.py
+++ src/transaction_api.py
@@ -21,7 +21,8 @@ def create_user(username: str, password: str):
 
     new_user = {"username": username, "password": password}
     # TODO(shawn): Check if username is in the database and return and error if it is
-    ...
+    if _RD.exists(username) == 1:
+        raise Exception('Username {} already exists. Please choose a different username.'.format(username))
 
     # Generate a new public and private key used to sign transactions.
     (pubkey, private) = rsa.newkeys(512)
@@ -62,8 +63,12 @@ def delete_user():
     Example: curl 0.0.0.0:5000/user/delete?username=myname
     """
     # TODO(shawn): Remove the user from the database
-    ...
-
+    user_to_delete = request.args.get('username',type=str)
+    if _RD.exists(user_to_delete) == 1:
+        _RD.del(user_to_delete)
+    else
+        raise Exception('User {} does not exist.'.format(user_to_delete)
+    return "User deleted."
 
 @app.route("/transaction/new", methods=["POST"])
 def create_transaction():
@@ -78,13 +83,26 @@ def create_transaction():
     # Take the from's username and password and extract the private to sign the transaction.
     transaction = request.get_json(force=True)
     from_username = transaction["from"]["username"]
-
+    from_password = transaction["from"]["password"]
     # TODO(shawn): Check that the from user exists and the password is right
-    ...
+    if _RD.exists(from_username) == 1:
+        info = pickle.loads(_RD.get(from_username))
+        actual_from_password = info['password']
+        if from_password != actual_from_password:
+            raise Exception('Submitted sender user password {} is incorrect.'.format(from_password))
+    else
+        raise Exception('Sender user {} does not exist.'.format(from_username))
     # TODO(shawn): Check that the to user exists
-    ...
+    to_username = transaction["to"]["username"]
+    to_password = transaction["to"]["password"]
+    if _RD.exists(to_username) == 1:
+        to_info = pickle.loads(_RD.get(to_username))
+        actual_to_password = to_info["password"]
+        if to_password != actual_to_passowrd:
+            raise Exception('Submitted recipient user password {} is incorrect.'.format(to_password))
+    else
+        raise Exception('Submitted recipient user {} does not exist.'.format(to_username))
 
-    info = pickle.loads(_RD.get(from_username))
     private_key = rsa.PrivateKey.load_pkcs1(info["private_key"])
     signature = rsa.sign(json.dumps(transaction).encode(), private_key, "SHA-256")
     # Update the transaction with the signed payment and the timestamp
